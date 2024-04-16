eb817ffbbff00e2f5b6583969f538c5185bbf36b
MAX-DANTAS <164426597+MAX-DANTAS@users.noreply.github.com>
2024-04-16 14:39:56 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello, World!")
+const express = require('express')
+
+const app = express()
+
+app.get('/', function (req, res) {
+
+  res.send('Hello World')
+
+})
+
+app.get('/oi', function (req, res) {
+
+    res.send('Olá, mundo')
+
+  })
+
+app.listen(3000)
