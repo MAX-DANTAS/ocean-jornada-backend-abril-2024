f148105751976424894c75281d17d6f3f2b295ba
MAX-DANTAS <164426597+MAX-DANTAS@users.noreply.github.com>
2024-04-17 14:44:19 +0000

    Endpoint Create criado

--- index.js
+++ index.js
@@ -21,4 +21,24 @@ app.get('/oi', function (req, res) {
   app.get('/item', function (req, res) {
     res.send(lista)
   })
+  // Endepoint Read By ID --> {get} /item/
+   app.get('/item/:id', function (req, res){
+    const id = req.params.id
+
+  const item = lista[id -1]
+
+  res.send(item)
+   })
+// Espeifcamos que o corpo da requisição será em JSON
+  app.use(express.json())
+
+//Endepoint Create --> [Post]/ item
+app.post('/item', function(req, res){
+
+  const item = req.body.nome
+
+  lista.push(item)
+  res.send('Item adicionado com sucesso ' + item)
+})
+
 app.listen(3000)
